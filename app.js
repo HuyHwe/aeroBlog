f9e883b0fa3a04abba3488e3e4c90db4abf844e2
Nguyễn Đức Huy <nguyenduchuy564@gmail.com>
2023-03-06 15:23:52 +0700

    add database connection

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const app = express();
+const env = require("dotenv");
+const {Pool} = require("pg");
+
+const poolDB = new Pool({
+    host: "localhost",
+    database: "aeroBlog",
+    user: "postgres",
+})
+env.config();
 
 const PORT = process.env.PORT;
-
+const connectionString = process.env.DATABASE_URL;
 app.listen(PORT, () => {
     console.log("listening to port: " + PORT);
 })
